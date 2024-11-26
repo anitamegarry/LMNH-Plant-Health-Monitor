3ad3ec4e55f684b0076e2efb639b6e0426a7c8ff
Anita Megarry <anitamegarry@gmail.com>
2024-11-26 20:04:22 +0000

    Added functions to extract_from_database.py

--- extract_from_database.py
+++ extract_from_database.py
@@ -3,12 +3,44 @@
 # pylint: disable=E1101
 
 import os
+import csv
 
 import pymssql
 from dotenv import load_dotenv
 import boto3
+from datetime import datetime
 
 load_dotenv()
+
+SCHEMA = os.getenv("SCHEMA_NAME")
+
+QUERY = f"""
+SELECT
+    recording.recording_id,
+    recording.recording_taken,
+    recording.last_watered,
+    recording.soil_moisture,
+    recording.temperature,
+    plant.plant_id,
+    species.plant_name,
+    country.country_name,
+    botanist.botanist_first_name,
+    botanist.botanist_last_name,
+    botanist.botanist_email,
+    botanist.botanist_phone_number
+FROM
+    {SCHEMA}.recording
+JOIN
+    {SCHEMA}.plant ON recording.plant_id = plant.plant_id
+JOIN
+    {SCHEMA}.species ON plant.species_id = species.species_id
+JOIN
+    {SCHEMA}.country ON plant.country_id = country.country_id
+JOIN
+    {SCHEMA}.botanist ON plant.botanist_id = botanist.botanist_id;
+"""
+
+CSV_FILE = f"{datetime.today().strftime('%Y-%m-%d')}_plant_monitor_data.csv"
 
 
 def get_connection():
@@ -17,6 +49,35 @@ def get_connection():
                            user=os.getenv("DB_USER"),
                            server=os.getenv("DB_HOST"),
                            password=os.getenv("DB_PASSWORD"))
+
+
+def get_cursor(conn):
+    """Gets cursor"""
+    return conn.cursor()
+
+
+def extract_data() -> None:
+    """Extracts all data from database"""
+    conn = get_connection()
+    if not conn:
+        return None
+    try:
+        conn = get_connection()
+        cursor = get_cursor(conn)
+        cursor.execute(QUERY)
+        results = cursor.fetchall()
+        column_titles = [description[0] for description in cursor.description]
+        with open(CSV_FILE, mode="w", newline="", encoding="utf-8") as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerow(column_titles)
+            writer.writerows(results)
+        print("Data extracted successfully.")
+    except pymssql.Error as e:
+        print(f"Error executing query: {e}")
+        return None
+    finally:
+        conn.close()
+        print("Connection closed.")
 
 
 def get_client():
@@ -33,3 +94,4 @@ def get_client():
 
 if __name__ == "__main__":
     print(get_connection())
+    print(extract_data())
