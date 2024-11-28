d631f46c9349e07564fa8a5d57fc3e76e23989a5
jiuliangut <jiuliancarlo@gmail.com>
2024-11-28 17:01:33 +0000

    pylinted

--- pipeline/etl.py
+++ pipeline/etl.py
@@ -2,10 +2,10 @@
 import load
 
 
-def lambda_handler(event, context):
+def lambda_handler(event, context):  # pylint: disable=unused-argument
     """Calls load.main() when the Lambda is invoked on AWS"""
     try:
         load.main()
         return {"statusCode": 200, "body": "Successfully executed ETL pipeline"}
-    except Exception as e:
+    except Exception as e:  # pylint: disable=broad-except
         return {"statusCode": 500, "body": f"Error occurred: {str(e)}"}
