0608230140e1cc801b4e28ba09c1c01bd0274fa6
jiuliangut <jiuliancarlo@gmail.com>
2024-11-28 16:52:02 +0000

    add lambda_function

--- pipeline/etl.py
+++ pipeline/etl.py
@@ -1,5 +1,11 @@
 """Script that emulates the ETL pipeline in a single file."""
 import load
 
-if __name__ == "__main__":
-    load.main()
+
+def lambda_handler(event, context):
+    """Calls load.main() when the Lambda is invoked on AWS"""
+    try:
+        load.main()
+        return {"statusCode": 200, "body": "Successfully executed ETL pipeline"}
+    except Exception as e:
+        return {"statusCode": 500, "body": f"Error occurred: {str(e)}"}
