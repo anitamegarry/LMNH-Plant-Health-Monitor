f0f16269772941e696755aa24e05bb02179e3b10
Anita Megarry <anitamegarry@gmail.com>
2024-11-29 15:37:46 +0000

    Updated dashboard ecs

--- terraform/dashboard_ecs.tf
+++ terraform/dashboard_ecs.tf
@@ -65,3 +65,17 @@ resource "aws_ecs_task_definition" "c14_plant_practitioners_task_definition" {
     execution_role_arn = data.aws_iam_role.ecs_task_execution_role.arn
     task_role_arn      = data.aws_iam_role.ecs_task_execution_role.arn
 }
+
+resource "aws_ecs_service" "c14_plant_practitioners_service" {
+  name            = "c14-plant-practitioners-service"
+  cluster         = aws_ecs_cluster.c14-ecs-cluster.id
+  task_definition = aws_ecs_task_definition.c14_plant_practitioners_task_definition.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = ["subnet-0497831b67192adc2", "subnet-0acda1bd2efbf3922", "subnet-0465f224c7432a02e"]
+    security_groups  = [data.aws_security_group.c14_plant_practitioners_security_group.id]
+    assign_public_ip = true
+  }
+}
